45a6e868657069337bfd7c347d23f1310c6b3b04
ghkwns5191 <ghkwns5191@gmail.com>
2022-09-21 10:48:19 +0900

    인테리어 추가

--- demo/src/frontend/src/views/interiorView.vue
+++ demo/src/frontend/src/views/interiorView.vue
@@ -3,16 +3,16 @@
         <body>
 
 <div class="container">
-  <p style="margin-left: 73%; font-size: 25px;" class="">product portpolio</p>
+  <p style="margin-top: 30px; font-size: 25px;">Interior</p>
     <!--<h1 style="margin-left: 78%;">Product interior</h1>-->
     </div>
-    <hr width="1350px">
-<div style="margin-left: 20%;  margin-top: 50px;">   
-<table cellpadding="75">
+    <hr style="width: 1300px; margin-left: 300px;">
+<div style="margin-left: 430px;  margin-top: 50px;">   
+<table cellpadding="55">
 <tr>
 <td>
  <!-- <strong p class="lead text-muted">Araucaria heterophylla</strong></p><br>-->
-    <a href="#"> <img :src="require(`../assets/interior_img/${interior1}.jpg`)" alt="인테리어1" style="width: 400px;" > </a><br><br>
+    <img :src="require(`../assets/interior_img/${interior1}.jpg`)" alt="인테리어1" style="height: 600px; width: 400px;" @click="nowRepairing"><br><br>
         <div class="card-body">
             <strong p class="card-text"> Araucaria heterophylla</strong>
             <div class="d-flex justify-content-between align-items-center">
@@ -22,7 +22,7 @@
         </div>
       </div></td>
 <td>
-   <a href="#"> <img :src="require(`../assets/interior_img/${interior2}.png`)" alt="인테리어2" style="width: 400px;" > </a><br><br>
+   <img :src="require(`../assets/interior_img/${interior2}.png`)" alt="인테리어2" style="height: 600px; width: 400px;" @click="nowRepairing"><br><br>
         <div class="card-body">
             <p><strong p class="card-text"> Olea europaea L.</strong></p>
             <div class="d-flex justify-content-between align-items-center">
@@ -33,12 +33,12 @@
 </div> 
 
 
-
+<div style="margin-left: 400px;"> 
 <table cellpadding="75">
 <tr>
 <td>
   <div class="container">
-      <a href="#"> <img :src="require(`../assets/interior_img/${interior3}.png`)" alt="인테리어3" style="width: 400px;" > </a><br><br>
+      <img :src="require(`../assets/interior_img/${interior3}.png`)" alt="인테리어3" style="height: 600px; width: 400px;" @click="nowRepairing"><br><br>
           <div class="card-body">
             <p><strong p class="card-text"> Myrtillocactus geometrizans</strong></p>
             <div class="d-flex justify-content-between align-items-center">
@@ -47,7 +47,7 @@
 
 <td>
 <div class="container">
-      <a href="#"> <img :src="require(`../assets/interior_img/${interior4}.png`)" alt="인테리어2" style="width: 400px;" > </a><br><br>
+      <img :src="require(`../assets/interior_img/${interior4}.png`)" alt="인테리어2" style="height: 600px; width: 400px; margin-left: -60px;" @click="nowRepairing"><br><br>
           <div class="card-body">
             <p><strong p class="card-text"> Cereus jamacaru</strong></p>
             <div class="d-flex justify-content-between align-items-center">
@@ -56,17 +56,13 @@
         </td>
         </tr>
 </table>
+</div>
 
 
 
 
  <!-- 페이징 -->
- <p id="paging">
-  <button>1</button>
-  <button>2</button>
-  <button style="color:white; background-color:rgb(22, 160, 133)">3</button>
-  <button>4</button>
-</p>
+ 
 </body>
     </div>
 </template>
@@ -81,6 +77,11 @@
                 interior3: "in2",
                 interior4: "in3",
             }
+        },
+        methods: {
+            nowRepairing(){
+                alert("해당 페이지는 현재 준비 중입니다.")
+            }
         }
     }
 </script>
@@ -91,6 +92,10 @@
         transform:scale(1.05);
         transition: transform.10s;
         }
+
+    img {
+        cursor: pointer;
+    }
 
     @import "../css/main_customer.css";
 </style>
